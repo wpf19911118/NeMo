a1ab67c60e6e5df774d8a5553df0517f79020e8f
Yang Zhang <yangzhang@nvidia.com>
2020-02-27 15:10:15 -0800

    fix nlp import
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo/collections/nlp/__init__.py
+++ nemo/collections/nlp/__init__.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 # =============================================================================
 
+import nemo
 import nemo.collections.nlp.callbacks
 import nemo.collections.nlp.data
 import nemo.collections.nlp.nm
